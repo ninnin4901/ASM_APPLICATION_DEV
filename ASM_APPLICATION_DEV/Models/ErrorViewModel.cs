f3fc981573ebe9178d34ce7b67cf5f4ddeab5eaf
ninnin4901 <Vinhduong04092001@gmail.com>
2023-04-26 21:45:10 +0700

    Add project files.

---
+++ ASM_APPLICATION_DEV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASM_APPLICATION_DEV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
